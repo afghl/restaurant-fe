2c6eb8848da1c6d9678bfbf88fac4eb31326d51c
afghl <airport.0930@gmail.com>
2017-10-19 09:53:18 +0800

    init basic structure

---
+++ server.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack')
+var webpackDevMiddleware = require('webpack-dev-middleware')
+var webpackHotMiddleware = require('webpack-hot-middleware')
+var proxy = require('http-proxy-middleware')
+if (process.env.NODE_ENV === 'production') {
+  var config = require('./webpack.prod.config')
+} else {
+  var config = require('./webpack.config')
+}
+var express = require('express')
+
+var app = new express()
+var port = process.env.NODE_ENV === 'production' ? 3001 : 3000
+
+var compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
+app.use('/api', proxy({target: 'http://localhost:9090', changeOrigin: true}));
+
+app.set('view engine', 'ejs')
+
+app.use('/images', express.static('images'))
+app.use('/fonts', express.static('fonts'))
+app.use('/page', function(req, res) {
+  res.render('../index', { data: req.query })
+})
+
+app.listen(port, function(error) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+  }
+})
---
+++ webpack.prod.config.js
@@ -0,0 +1,48 @@
+var path = require('path')
+var webpack = require('webpack')
+
+var js = {
+  name: 'js',
+  devtool: 'source-map',
+  entry: [
+    // 'webpack-hot-middleware/client',
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+       compressor: {
+         warnings: false
+       }
+     })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: [ 'babel' ],
+        exclude: /node_modules/,
+        include: __dirname
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader: 'file-loader'
+      }
+    ]
+  }
+}
+
+module.exports = js
