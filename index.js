805e946bdde81cf9649f22513495e739f6280b2f
afghl <airport.0930@gmail.com>
2017-10-21 15:35:35 +0800

    require jquery

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import 'babel-polyfill'
 import './sass/main.scss'
 import moment from 'moment'
-import * from '..components/api'
+// import * from './components/api'
+import $ from 'jquery'
 
 moment.locale('zh-cn')
+
+$(() => {
+  console.log('helloworld');
+})
