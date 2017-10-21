a28544ce3c6a518439c7ad84a7ae594c187627dc
afghl <airport.0930@gmail.com>
2017-10-21 15:33:33 +0800

    add api

--- components/test_module.js
+++
@@ -1,1 +0,0 @@
-export const test = (a, b) => a(b)
--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import 'babel-polyfill'
 import './sass/main.scss'
 import moment from 'moment'
-import { test } from './components/test_module'
+import * from '..components/api'
 
 moment.locale('zh-cn')
-
-console.log(test);
-test(console.log, 'helloword2')
-console.log('helloword');
